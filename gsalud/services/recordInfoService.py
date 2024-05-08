704744bfb0795f305c82afdb8a6df6535a76f701
Martin <caceresmartin22@gmail.com>
2024-05-08 14:04:15 +0200

    V2.1.3
    Existing case with no assignment data

--- gsalud/services/recordInfoService.py
+++ gsalud/services/recordInfoService.py
@@ -1,3 +1,5 @@
+from typing import List, Dict
+from django.core.exceptions import ObjectDoesNotExist
 from gsalud.serializers import RecordInfoSerializer
 from django.db import transaction
 from gsalud.models import RecordInfo, RecordsInfoUsers
@@ -13,13 +15,26 @@ def insertRecordsCases(newRecordInfoData):
             print("Error in serializer validation:", serializer.errors)
 
 
-def updateRecordsCases(updateRecordsInfoIds, updateRecordsInfoData):
+def updateRecordsCases(updateRecordsInfoIds: List[int], updateRecordsInfoData: List[Dict[str, str]]) -> None:
+    """
+    Updates the RecordInfo instances in the database based on the provided updateRecordsInfoIds and updateRecordsInfoData.
+    If a RecordInfo instance with the given update_id is found, it is updated with the provided data using the RecordInfoSerializer.
+    If the instance is not found, a new RecordInfo instance is inserted.
+
+    Args:
+    - updateRecordsInfoIds: A list of integers representing the IDs of the RecordInfo instances to be updated.
+    - updateRecordsInfoData: A list of dictionaries containing the updated data for each RecordInfo instance.
+
+    Returns:
+    - None
+    """
     with transaction.atomic():
         for update_id, update_data in zip(updateRecordsInfoIds, updateRecordsInfoData):
-            record_info_instance = RecordInfo.objects.get(
-                id_record__exact=update_id)
-            serializer = RecordInfoSerializer(
-                instance=record_info_instance, data=update_data)
+            try:
+                record_info_instance = RecordInfo.objects.get(id_record__exact=update_id)
+            except ObjectDoesNotExist:
+                record_info_instance = RecordInfo()
+            serializer = RecordInfoSerializer(instance=record_info_instance, data=update_data)
             if serializer.is_valid():
                 serializer.save()
             else:
